c67710ef348c972cdc5509c2111d6622e0548bb0
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-16 01:03:15 +0000

    [Finish] Applicationtask3

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,12 +5,12 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
   
   def destroy
    PostComment.find(params[:id]).destroy
-   redirect_to book_path(params[:book_id])
+   redirect_to request.referer
   end 
   
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_120517) do
+ActiveRecord::Schema.define(version: 2023_03_16_003905) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2023_03_15_120517) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "post_image_id"
+    t.integer "book_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
