f233ae973ad845ba91bb4b16a576ec67b8f7ad27
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-17 12:53:57 +0000

    [Finish] Applicationtask4

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,17 +4,5 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   #1人のユーザーはたくさんのユーザーからフォロー(followed)される
   belongs_to :followed, class_name: "User"
-  #フォローいた時の処理
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
-  end
-  #フォローを外す時の処理
-  def unfollow(user_id)
-    relationships.find(follwed_id: user_id).destroy
-  end
-  #フォローしているか判断
-  def following?(user)
-    followings.include?(user)
-  end
   
 end
