ed6a2120573aab8bcb44bcf0e14a19750eee77b1
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-18 08:47:17 +0000

    [Finish] Applicationtask5

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end 
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
