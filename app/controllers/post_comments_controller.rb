e5f1cc2e45a53df4a41bad1c5ac95bffba43860d
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-21 06:03:18 +0000

    [Finish] Applicationtask6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,12 +5,14 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @post_comment = PostComment.new
   end
   
   def destroy
-   PostComment.find(params[:id]).destroy
-   redirect_to request.referer
+    PostComment.find(params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    @post_comment = PostComment.new
   end 
   
   private
