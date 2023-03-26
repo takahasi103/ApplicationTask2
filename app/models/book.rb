1f8f4cb13e8530392e215ccf6b436de7b1793e6b
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-26 08:16:52 +0000

    [Save] user_show

--- app/models/book.rb
+++ app/models/book.rb
@@ -28,9 +28,14 @@ class Book < ApplicationRecord
   #今日
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   #昨日
-  scope :created_yesterday, -> { where(created_at: Time.zone.yesterday.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2_days_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3_days_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4_days_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5_days_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6_days_ago, -> { where(created_at: 6.day.ago.all_day) }
   #今週
-  scope :created_week, -> { where(created_at: Time.current.all_week) }
+  scope :created_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   #先週
-  scope :created_week_ago, -> { where(created_at: Time.current.last_week.all_week) }
+  scope :created_week_ago, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 end
