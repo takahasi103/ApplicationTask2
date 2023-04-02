465d51ac9d5f687ebd9e50405b8a8b46f79d61ff
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-04-02 12:53:34 +0000

    [Save] raty in the process of adding

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery";
-import "popper.js";
-import "bootstrap";
-import "../stylesheets/application" 
-import '@fortawesome/fontawesome-free/js/all'
+import jQuery from "jquery"
+import "popper.js"
+import "bootstrap"
 
+import '@fortawesome/fontawesome-free/js/all';
+import "../stylesheets/application"
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+global.$ = jQuery;
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_29_112726) do
+ActiveRecord::Schema.define(version: 2023_04_02_112956) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2023_03_29_112726) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "direct_messages", force: :cascade do |t|
